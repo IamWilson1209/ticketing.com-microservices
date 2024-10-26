656a3ba4e9c6b766cd020077caa9042abf070bea
WeiShiuan <zenfonlee@gmail.com>
2024-10-26 14:24:25 +0800

    add start up msg to order service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,7 @@ import { ExpirationCompleteListener } from './events/listeners/expiration-comple
 import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 const start = async () => {
+  console.log('Orders service Starting...');
   // Check if JWT_KEY is defined in environment variables
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
