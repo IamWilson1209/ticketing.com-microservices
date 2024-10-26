615846236fb0d5a68c550e3e81853562a6ec8cc5
WeiShiuan <zenfonlee@gmail.com>
2024-10-26 21:54:20 +0800

    nothing imp

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Auth service is starting up successfully......damn')
+  console.log('Auth service is starting up successfully......damnnnnn')
   // Check if JWT_KEY is defined in environment variables
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
