451df46c2c1c18314aa02a5d3375c775a2fd062b
WeiShiuan <zenfonlee@gmail.com>
2024-10-26 22:18:24 +0800

    tiny change from dev

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,7 @@ import { ExpirationCompleteListener } from './events/listeners/expiration-comple
 import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 const start = async () => {
-  console.log('Orders service is starting......');
+  console.log('hi from dev')
   // Check if JWT_KEY is defined in environment variables
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,6 +6,7 @@ import { OrderCancelledListener } from './events/listener/order-cancelled-listen
 
 const start = async () => {
   // Check if JWT_KEY is defined in environment variables
+  console.log('hi from dev')
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
