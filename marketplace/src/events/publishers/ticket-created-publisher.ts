ff271f9c1dfdbc237ebac33c12f9a86913034ff3
WeiShiuan <zenfonlee@gmail.com>
2024-11-16 19:18:27 +0800

    delete redundant files

--- marketplace/src/events/publishers/ticket-created-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, Subjects, TicketCreateEvent } from "@weitickets/common";
-
-export class TicketCreatedPublisher extends Publisher<TicketCreateEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated;
-}
--- marketplace/src/events/publishers/ticket-updated-publisher.ts
+++
@@ -1,6 +0,0 @@
-import { Publisher, Subjects, TicketUpdatedEvent } from "@weitickets/common";
-import { natsWrapper } from "../../nats-wrapper";
-
-export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
-  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
-}
